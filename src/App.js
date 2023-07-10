fbb5caff8c2570e6717a2e7f1fee07beb4b63d8d
ranjitha <ranjisakthi97@gmail.com>
2023-07-10 17:13:51 +0530

    Todo_list_list_&_key

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Conent from './Content';
+import Footer from './Footer';
+
+function App() { 
+  return (
+    <div className='App'>
+      <Header/>
+      <Conent/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
