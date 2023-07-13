621ed55ddcd61cdf63b09ce3fd0841cea3461508
ranjitha <ranjisakthi97@gmail.com>
2023-07-13 16:32:52 +0530

    Search -React

--- src/App.js
+++ src/App.js
@@ -2,26 +2,34 @@ import Header from './Header';
 import Conent from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
- 
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
+
 function App() {
-  const [ items, setItems] = useState(
-    [
-      {
-        id: 1,
-        checked: true,
-        item: "Prac1 Coding"
-      },
-      {
-        id: 21,
-        checked: false,
-        item: "Play video"
-      },
-      {
-        id: 33,
-        checked: false,
-        item: "Read React"
-      },
-    ])
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('todo_list')))
+
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const addNewItem = {
+      id,
+      checked: false,
+      item
+    }
+    const listItems = [...items, addNewItem];
+    setItems(listItems);
+    localStorage.setItem("todo_list", JSON.stringify(listItems));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem)
+    setNewItem('')
+  }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ?
@@ -38,13 +46,21 @@ function App() {
   return (
     <div className='App'>
       <Header title="Ranjitha" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit} />
+      <SearchItem
+        search={search}
+        setSearch={setSearch} />
       <Conent
-        items= {items}
-        handleCheck = {handleCheck}
-        handleDelete = {handleDelete  }
+        items={items.filter((item) => (item.item).toLowerCase()
+          .includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
-      <Footer 
-      length = {items.length} />
+      <Footer
+        length={items.length} />
     </div>
   );
 }
